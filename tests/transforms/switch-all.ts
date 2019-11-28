0324e3d573abf850f23bfe85b00bf9f4b9e06910
Surma <surma@surma.link>
2019-11-28 13:57:57 +0000

    Add another test

--- tests/transforms/switch-all.ts
+++ tests/transforms/switch-all.ts
@@ -70,4 +70,30 @@ Mocha.describe("switch-all()", function() {
 
     chai.expect(await listP).to.deep.equal([11, 12, 21, 22, 23]);
   });
+
+  Mocha.it("handles closing streams correctly", async function() {
+    const o1 = external();
+    const o2 = external();
+    const outerO = external();
+
+    const listP = collect(outerO.observable.pipeThrough(switchAll()));
+
+    outerO.next(o1.observable);
+    await waitTicks();
+    safeNext(o1, 11);
+    await waitTicks();
+    safeNext(o1, EOF);
+    await waitTicks();
+    outerO.next(o2.observable);
+    outerO.next(EOF);
+    await waitTicks();
+    safeNext(o2, 21);
+    await waitTicks();
+    safeNext(o2, 22);
+    await waitTicks();
+    safeNext(o2, EOF);
+    await waitTicks();
+
+    chai.expect(await listP).to.deep.equal([11, 21, 22]);
+  });
 });
