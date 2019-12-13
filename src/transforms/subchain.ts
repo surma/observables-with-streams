61bd121954c0e1c3f4424b32ef8b9cc89b6d06cd
Surma <surma@surma.link>
2019-12-13 14:00:14 +0000

    Implement subchain()

---
+++ src/transforms/subchain.ts
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2019 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Observable, Transform } from "../types.js";
+
+/**
+ * Returns a `Transform` that applies `f` to the observable.
+ *
+ * @example
+ * `subchain()` can be used to create logical groups in a longer chain
+ * or to make parts of a chain reusable.
+ *
+ * ```typescript
+ * ows
+ *   .fromEvent(button, "click")
+ *   .pipeThrough(
+ *     ows.subchain(o =>
+ *       o
+ *         .pipeThrough(ows.map(() => fetch("/stockData")))
+ *         .pipeThrough(ows.map(r => r.json()))
+ *         .pipeThrough(ows.filter(data => data.tags.contains(importTag)))
+ *     )
+ *   )
+ *   .pipeTo(
+ *     ows.discard(data => {
+ *       // ...
+ *     })
+ *   );
+ * ```
+ *
+ * @typeparam S Type of items emitted by the original observable.
+ * @typeparam T Type of items returned by the observable `f` returns.
+ * @param f Function that will be applied to the observable.
+ * @returns Transform that applies `f` to the observable.
+ */
+export function subchain<S, T>(
+  f: (x: Observable<S>) => Observable<T>
+): Transform<S, T> {
+  const { readable, writable } = new TransformStream();
+  return {
+    writable,
+    readable: f(readable)
+  };
+}
---
+++ tests/transforms/subchain.ts
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2019 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { range, subchain, filter, map, collect } from "../../src/index.js";
+
+Mocha.describe("subchain()", function() {
+  Mocha.it("runs the subchain", async function() {
+    const list = await collect(
+      range(1, 10).pipeThrough(
+        subchain(o =>
+          o.pipeThrough(filter(v => v % 2 == 0)).pipeThrough(map(v => v * 3))
+        )
+      )
+    );
+
+    chai.expect(list).to.deep.equal([6, 12, 18, 24, 30]);
+  });
+});
